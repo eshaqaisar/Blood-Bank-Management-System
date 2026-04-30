0bda25e4a290b9b6feba62695883f3dcba728bcd
aliwaseem <waseemhtc496@gmail.com>
2026-04-30 23:02:28 +0500

    registrationfrom,donortable,dashboared, person,donor,patient.hand cpp

--- Views/DonorTable.h
+++ Views/DonorTable.h
@@ -1,25 +1,45 @@
-#ifndef DONORTABLE_H
-#define DONORTABLE_H
+#ifndef DONORLISTFORM_H
+#define DONORLISTFORM_H
 
-#include <QMainWindow>
+#include <QWidget>
 #include <QTableWidget>
-#include "ui_DonorTable.h"
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QComboBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
 
-class DonorTable : public QMainWindow
-{
+
+
+
+class DonorListForm : public QWidget {
     Q_OBJECT
 
 public:
-    DonorTable(QWidget* parent = nullptr);
-    ~DonorTable();
-    void loadData(); // File se data parhne ke 
+    explicit DonorListForm(QWidget* parent = nullptr);
+    ~DonorListForm();
+
+private slots:
+    void onSearchChanged(const QString& text); // Filter table as user types
+    void onDeleteClicked();                    // Delete selected row
+    void onAddDonorClicked();                  // Open DonorRegistrationForm
+    void onViewProfileClicked();               // Show popup with donor details + compatibility
+    void onFilterByBloodGroup(const QString& bg); // Filter table by blood group
 
 private:
-    Ui::DonorTableClass* ui;
-   
-    
-private slots:
-    void filterTable(const QString& arg1);
+    QTableWidget* tblDonors;
+    QLineEdit* txtSearch;
+    QComboBox* cmbFilter;      // Filter by blood group
+    QPushButton* btnDelete;
+    QPushButton* btnAdd;
+    QPushButton* btnViewProfile;
+    QLabel* lblCount;       // Shows "Showing X donors"
+
+    void setupUI();
+    void loadDonors();           // Read donors.txt and fill table
+    void applyStyle();
+    void filterTable(const QString& text, const QString& bloodGroup);
 };
 
-#endif
+#endif // DONORLISTFORM_H
