8e9d25f980d64d1b62a19e831ffc9d2f9c5c1303
Noorfatima-star <noorfatimaimam96@gmail.com>
2026-05-01 17:19:12 +0500

    removed extra date function

--- Models/BloodRequest.cpp
+++ Models/BloodRequest.cpp
@@ -49,9 +49,7 @@ BloodRequest BloodRequest::fromFileString(const QString& line) {
         req.requestDate = QDate::fromString(p[6].trimmed(), "yyyy-MM-dd");
     return req;
 }
-QDate BloodRequest::getRequestDate() const {
-    return requestDate; // make sure this variable exists
-}
+
 
 // file handling
 
